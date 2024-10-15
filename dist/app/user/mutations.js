cfa5b252c6301ab6be55af76998f4bbb1d3e1123
Sri Om Sharan <sriomsharan.personal@gmail.com>
2024-10-15 22:50:18 +0530

    Delete dist directory

--- dist/app/user/mutations.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mutation = void 0;
-exports.mutation = `#graphql
-    createUser(email:String!,firstName:String!,dob:String!,lastName:String!): String!
-`;
